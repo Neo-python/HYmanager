83eac0b4582604f078693f5b6ed8cd9ad7359621
neo <g602049338@icloud.com>
2019-11-25 17:52:26 +0800

    500错误拦截

--- main.py
+++ main.py
@@ -16,5 +16,11 @@ def view_error(error):
     return jsonify(error.info)
 
 
+@app.errorhandler(500)
+def server_error(error):
+    """视图错误"""
+    return jsonify({"error_code": 5099, "message": str(error)})
+
+
 if __name__ == '__main__':
     app.run(port=8093, host='0.0.0.0', debug=True)
