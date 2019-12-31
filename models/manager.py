7176281c6025ed3742a800555c4714f69cfb8305
neo <g602049338@icloud.com>
2020-01-01 00:40:33 +0800

    去除消息相关功能

--- models/manager.py
+++ models/manager.py
@@ -18,8 +18,8 @@ class DriverOrderScheduleLog(business.DriverOrderScheduleLogBase):
     """驾驶员订单进度"""
 
 
-class DriverSystemMessage(user.DriverSystemMessageBase):
-    """驾驶员系统消息"""
+# class DriverSystemMessage(user.DriverSystemMessageBase):
+#     """驾驶员系统消息"""
 
 
 class FactoryOrder(business.OrderBase):
