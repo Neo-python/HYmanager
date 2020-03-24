2d9a5305b30c0b55170b231ef38bb6b3e1305291
neo <g602049338@icloud.com>
2020-03-24 22:40:30 +0800

    订单排序调整

--- views/manager/factory.py
+++ views/manager/factory.py
@@ -45,7 +45,7 @@ def factory_order_list():
     if form.schedule.data is not None:
         query = query.filter_by(schedule=form.schedule.data)
 
-    query.order_by(FactoryOrder.id.desc())
+    query.order_by(FactoryOrder.id)
 
     paginate = query.paginate(form.page.data, form.limit.data, error_out=False)
 
